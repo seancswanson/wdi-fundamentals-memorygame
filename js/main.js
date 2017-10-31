3302d85a97252b18f2a27c8ded836ac650dbb077
seanthaswan <owneruser@Owners-MacBook-Pro.local>
2017-10-31 16:30:46 -0700

    updated game logic, troubleshooted, squashed the bug

--- js/main.js
+++ js/main.js
@@ -1,78 +1,62 @@
-var cards = [
-{
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
-},
-{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
-},
-{
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "king-of-diamonds.png"
-}
+// set variable of card objects
+var cards = [{
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
 ];
 
+
+// set variable to hold 'flipped' cards to later reference for matches
 var cardsInPlay = [];
 
+
+// function to check card index from cardsInPlay for match.
 var checkForMatch = function() {
-	this.setAttribute("src", cards[cardId].cardImage)
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
-} else {
-	console.log("Sorry, try again");
-}
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+    } else {
+        console.log("Sorry, try again");
+    }
 }
 
+// function to execute when clicking on card IMG from the created board
 var flipCard = function() {
-
-var cardId = this.getAttribute(data-id);
-// 	var cardOne = cards[0];
-
-// cardsInPlay.push(cardOne);
-
-// var cardTwo = cards[2];
-
-// cardsInPlay.push(cardTwo);
-
-// console.log("User flipped " + cardsInPlay[0]);
-// console.log("User flipped " + cardsInPlay[1]);
-
-console.log("User flipped " + cards[cardId].rank);
-
-cardsInPlay.push(cards[cardId].rank);
-
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-
-// if (cardsInPlay.length === 2) {
-// 	if (cardsInPlay[0] === cardsInPlay[1]) {
-// 		alert("You found a match!");
-// 	}
-// 	else {
-// 		alert("Sorry, try again.");
-// 	}
-// }
-
+    var cardId = this.getAttribute("data-id");
+    // console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute("src", cards[cardId].cardImage);
+    if (cardsInPlay.length === 2) {
+    	checkForMatch();
+    }
 }
 
-var createBoard = function(){
-	for (var i = 0; i < cards.length; i++){
-		var cardElement = document.createElement("img");
-		cardElement.setAttribute("src", "images/back.png");
-		cardElement.setAttribute(data-id, cardElement[i]);
-		cardElement.addEventListener("click", flipCard);
-		document.getElementById("game-board").appendChild(cardElement);
-	};
+// function to create 4 card images which listen for a click.
+var createBoard = function() {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement("img");
+        cardElement.setAttribute("src", "images/back.png");
+        cardElement.setAttribute("data-id", i);
+        cardElement.addEventListener("click", flipCard);
+        document.getElementById("game-board").appendChild(cardElement);
+    };
 }
 
 
-checkForMatch();
+
+createBoard();
